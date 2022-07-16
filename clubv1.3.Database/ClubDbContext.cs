ae8047c35918d2f74874ed245e38ca3c4f5acca0
milanDimitrijevic <93523214+milanDimitrijevic@users.noreply.github.com>
2022-07-16 17:27:31 +0200

    Add files via upload

---
+++ clubv1.3.Database/ClubDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Clubv1._3.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace clubv1._3.Database
+{
+    public class ClubDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Serie> Series { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Renting> Rents { get; set; }
+
+        public ClubDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
